06b056e193572da368381274a6bdc65fa5f246ff
andersonvital <anderson.vital@ccc.ufcg.edu.br>
2017-08-13 18:19:46 +0000

    add funcao de retornar valor da matriz do tabuleiro.

--- main.pl
+++ main.pl
@@ -5,20 +5,24 @@
    ['~','~','~','~','~','~','~','~','~'], ['~','~','~','~','~','~','~','~','~'],
    ['~','~','~','~','~','~','~','~','~'] ).
 
-/*tiro(Tabuleiro, Linha, Coluna, NovoTabuleiro) :-
-  encontraPosicao(Tabuleiro, Linha, Coluna, Simbolo),
-  (Simbolo =:= '~' -> alteraValor(Tabuleiro, Linha, Coluna, '#', NovoTabuleiro);
+tiro(Tabuleiro, Linha, Coluna, NovoTabuleiro) :-
+  encontraSimboloNaMatriz(Tabuleiro, Linha, Coluna, Simbolo).
+  /*(Simbolo =:= '~' -> alteraValor(Tabuleiro, Linha, Coluna, '#', NovoTabuleiro);
    Simbolo =:= 'N' -> alteraValor(Tabuleiro, Linha, Coluna, 'X', NovoTabuleiro);
    Simbolo =:= '#' -> print("Você já atirou nessa posição!");
-   Simbolo =:= 'X' -> print "Você já atirou nessa posição!" ).
+   Simbolo =:= 'X' -> print "Você já atirou nessa posição!" ).*/
 
-alteraValor(Tabuleiro, Linha, Coluna, NovoValor, NovoTabuleiro) :- */
+encontraSimboloNaMatriz(Matriz, Linha, Coluna, Simbolo) :-
+  nth1(Linha, Matriz, ListaDaPos),
+  nth1(Coluna, ListaDaPos, Simbolo).
+
+/*alteraValor(Tabuleiro, Linha, Coluna, NovoValor, NovoTabuleiro) :-
+  */
 
 imprimeTabuleiroReal(MatrizTabuleiro) :-
   print('~°~°  TABULEIRO REAL °~°~'),nl,
   print('   1   2   3   4   5   6   7   8   9'),nl,
   imprimeLinhas(MatrizTabuleiro, 1).
-
 
 imprimeLinhas([], _).
 imprimeLinhas([H|T], Index) :-
